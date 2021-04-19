ec959b72c181da9c7bf1c088c5c85d83cb325601
Oliver Damsbjerg <olid@itu.dk>
2021-04-19 10:49:43 +0200

    language support

--- game_2/lang.js
+++ game_2/lang.js
@@ -1,7 +1,40 @@
-enExplanationtexts = {
-    practiceExplanation: "Thank you for wanting to help out in our project."
+explanationTexts = {
+    en: {
+        projectExplanation: "Thank you for wanting to help out in our project.",
+        practiceExplanation: "Good job completing the tutorial! Now before the game actually begins you will have a chance to familiarize yourself with all the spells and how it feels to progress in the game outside of a tutorial. So now you will be fighting seven Wizards. The Wizards are, as you learned in the tutorial, weak to all attacks, so don't worry about what spell might be best - try them all out!",
+        gameExplanation: "Here is a map of the enemies you will be fighting in the game as well as the abilities you will have available. Please rate on a scale from 1 (very unsure) to 10 (pretty certain) how certain you are that you will be able to defeat all the enemies",
+        endOfGame: "Thanks for playing! Please take a minute to answer our survey about your experience found",
+        confidenceText1: "not confident",
+        confidenceText2: "very confident"
+    },
+    dk: {
+        projectExplanation: "Tak fordi du vil hjælpe os med vores projekt.",
+        practiceExplanation: "øvelse",
+        gameExplanation: "spil",
+        endOfGame: "slut prut",
+        confidenceText1: "ikke sikker",
+        confidenceText2: "meget sikker"
+    }
 }
 
-dkExplanationTexts = {
-
+tutorialTexts = {
+    en: [
+        'Welcome to One-Shot Wizard! \n My name is: The-Handy-Dandy-Tutorial-Text and I will now guide you through how to play the game - please click on the arrow on the right to continue.',
+        'This is you. You are a Wizard.',
+        'This is your enemy. Each room in the game has one.',
+        'To defeat an enemy you must choose an attack from the ones available. \n Certain enemies are weak to certain attacks and it is up to you to figure out what attack will be effective against the enemy you are currently facing. \n \n But choose wisely! Because you only get ONE SHOT!',
+        'You are currently fighting this other Wizard. \n Wizards are, as we all know, very weak and frail, so don\'t worry about figuring out what spell is best - Anything would do the trick!',
+        'Now choose a spell.',
+        'When hit, enemies will try to resist the attack. If the resistance bar reaches the top, the enemy will recover from the spell and attack you back! \n Let\'s see how well the frail Wizard can resist your attack...', 
+        'Great job! You defeated the frail Wizard! \n \n Very impressive... \n \n When an enemy is defeated you will automatically move to the next room - so let\'s go there now!',
+        'Welcome to room 2!',
+        'BE CAREFUL!!! \n A NEW ENEMY APPROACHES! \n \n What a terrifying creature indeed... It must be slain!',
+        'But before you decide on what to attack with, take a look at your spells.',
+        'Notice Ice Spear is missing. \n You don\'t always have the same attacks available.',
+        'To see what spells you will have available for each enemy, look at the top of the screen. \n The portraits portray the enemies you will face and the icons below it indicate what spells you will have available then. \n This allows you to think ahead and plan the attack you wish to use against a given enemy. \n \n Or don\'t think ahead - I don\'t care.',
+        'Now I wish you good luck against the Slime! \n Because this is just the tutorial, whether you win or lose you will still move on to the next part after this Slime-battle. \n \n Have fun!',
+    ],
+    dk: [
+        
+    ]
 }
