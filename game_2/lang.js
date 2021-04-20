6c5de8101f252f8b271829d17bec7e3f4faf5e95
Oliver Damsbjerg <olid@itu.dk>
2021-04-20 15:17:24 +0200

    ljhb

--- game_2/lang.js
+++ game_2/lang.js
@@ -1,26 +1,26 @@
 explanationTexts = {
     en: {
-        projectExplanation: "Thank you for wanting to help out in our project. <br><br> Beware there is sound, it isn't integral to the experience, but we would recommend keeping it on. You can change the volume and mute the sound below the game window when it starts.",
-        practiceExplanation: "Good job completing the tutorial! Now before the game actually begins you will have a chance to familiarize yourself with all the spells and how it feels to progress in the game outside of a tutorial. So now you will be fighting seven Wizards. The Wizards are, as you learned in the tutorial, weak to all attacks, so don't worry about what spell might be best - try them all out!",
+        projectExplanation: "Thank you for wanting to help out in our project. <br><br> The game has music and sound effects that will start to play when you press the button below. Audio isn't integral to the experience, but we would recommend keeping it on. You can change the volume and/or mute the sound below the game window when it starts.",
+        practiceExplanation: "Good job completing the tutorial! Now before the game actually begins you will have a chance to familiarize yourself with all the spells and how it feels to progress in the game outside of a tutorial. So now you will be fighting seven Wizards. The Wizards, as you learned in the tutorial, are weak to all attacks, so don't worry about what spell might be best - try them all out!",
         gameExplanation: "Here is a map of the enemies you will be fighting in the game as well as the abilities you will have available. Please rate on a scale from 1 (very unsure) to 10 (pretty certain) how certain you are that you will be able to defeat all the enemies.",
-        endOfGame: "Thanks for playing! Please take a minute to answer our survey about your experience found",
+        endOfGame: "Thank you for playing! Please take a minute to answer our survey about your experience.",
         confidenceText1: "Not confident",
         confidenceText2: "Very confident",
         explanationButton: "Click here when you're done",
-        surveyLinkText: " here",
+        surveyLinkText: " Click here",
         fireball: "Fireball",
         iceSpear: "Ice Spear",
         tornado: "Tornado"
     },
     dk: {
-        projectExplanation: "Tak fordi du vil hjælpe os med vores projekt. <br><br> Vær opmærksom på at der er lyd, det er ikke strengt nødvendigt for spillet, men vi ville anbefale at have lyd på. Du kan ændre lydstyrken eller slå den fra under spilvinduet når spillet starter.",
-        practiceExplanation: "Godt arbejde med at gennemføre introduktionen! Før spillet begynder vil vi lige give dig en chance for at blive bekendt med alle besværgelserne og hvordan spillet føles uden for guiden. Du skal kæmpe mod syv wizards. Wizards er som du lærte i introduktionen svage over alle angreb, så du skal ikke bekymre dig om hvilken besværgelse der er bedst - prøv dem alle!",
-        gameExplanation: "Her er et kort over de fjender som du vil møde i spillet, samt de evner som du vil have til rådighed. På en skala fra 1 (meget usikker) til 10 (meget sikker) hvor sikker du er på at du vil kunne besejre alle fjenderne.",
-        endOfGame: "Tak for du ville spille vores spil! Til slut vil vi spørge om du lige vil bruge et øjeblik på at svare på nogle spørgsmål omkring din oplevelse med spillet",
+        projectExplanation: "Tak fordi du vil hjælpe os med vores projekt. <br><br> Vær opmærksom på, at der er musik og lydeffekter i spillet, som vil starte, når du trykker på knappen herunder. Lyd er ikke strengt nødvendigt for oplevelsen, men vi ville anbefale at have lyd på. Du kan ændre lydstyrken og/eller slå den fra under spilvinduet, når spillet starter.",
+        practiceExplanation: "Godt arbejde, du har gennemført introduktionen! Før spillet begynder, vil vi lige give dig en chance for at blive bekendt med alle magiske angreb, samt hvordan spillet føles uden for introduktionen. Du skal kæmpe mod syv troldmænd. Troldmænd er, som du lærte i introduktionen, svage over alle angreb, så du skal ikke bekymre dig om hvilket angreb der er bedst - prøv dem alle!",
+        gameExplanation: "Her er et kort over de fjender som du vil møde i spillet, samt de evner du vil have til rådighed. På en skala fra 1 (meget usikker) til 10 (meget sikker) hvor sikker er du på at du vil kunne besejre alle fjenderne?",
+        endOfGame: "Tak for du ville spille vores spil! Til slut vil vi spørge, om du lige vil bruge et øjeblik på besvare nogle spørgsmål omkring din oplevelse med spillet.",
         confidenceText1: "Meget usikker",
         confidenceText2: "Meget sikker",
         explanationButton: "Tryk her når du er færdig",
-        surveyLinkText: " her",
+        surveyLinkText: " Klik her",
         fireball: "Ildkugle",
         iceSpear: "Issyl",
         tornado: "Tornado"
@@ -29,35 +29,69 @@ explanationTexts = {
 
 tutorialTexts = {
     en: [
-        'Welcome to One-Shot Wizard! \n My name is: The-Handy-Dandy-Tutorial-Text and I will now guide you through how to play the game - please click the arrow to my right to continue.',
-        'This is you. You are a Wizard.',
-        'This is your enemy. Each room in the game has one.',
-        'To defeat an enemy you must choose an attack from the ones available. \n Certain enemies are weak to certain attacks and it is up to you to figure out what attack will be effective against the enemy you are currently facing. \n \n But choose wisely! Because you only get ONE SHOT!',
-        'You are currently fighting this other Wizard. \n Wizards are, as we all know, very weak and frail, so don\'t worry about figuring out what spell is best - anything would do the trick!',
-        'Now choose a spell.',
-        'When hit, enemies will try to resist the attack. If the resistance bar reaches the top, the enemy will recover from the spell and attack you back! \n Let\'s see how well the frail Wizard can resist your attack...', 
-        'Great job! You defeated the frail Wizard! \n \n Very impressive... \n \n When an enemy is defeated you will automatically move to the next room - so let\'s go there now!',
+        'Welcome to One-Hit Wizard! \n My name is: The-Handy-Dandy-Tutorial-Text and I will now guide you through how to play the game - please click the arrow to my right to continue.',
+        'This is you. You are a Wizard. Wizards have strong offensive capabilities, but they lack defense, and you have set out on a quest to change that. \n \n You have heard rumors of a magical shield hidden in this temple that can prevent all harm - meaning it could even prevent a Wizard from dying to a single hit, as they are known to do.',
+        'To get the shield you must defeat all enemies within the temple. \n \n This is one such enemy. Each room in the game has one.',
+        'It is up to you to choose the one attack of the ones available that can defeat your enemy. \n \n But choose wisely! Because you only get one chance...',
+        'You are currently facing an evil Wizard. \n Due to the lack of defense, fights between wizards tend to come down to who goes first. Thankfully in One-Hit Wizard, you always go first.',
+        'Now choose an attack. All of them are fit for fighting another Wizard.',
+        'When hit, enemies will try to resist the attack. If the resistance bar reaches the top, the enemy will recover from the spell and attack you back! \n Let\'s see how well the evil Wizard can resist your attack...', 
+        'Great job! You defeated the evil Wizard! \n \n When an enemy is defeated you will automatically move to the next room - so let\'s go there now!',
         'Welcome to room 2!',
-        'BE CAREFUL!!! \n A NEW ENEMY APPROACHES! \n \n What a terrifying creature indeed... It must be slain!',
-        'But before you decide on what to attack with, take a look at your spells.',
-        'Notice Ice Spear is missing. \n You don\'t always have the same attacks available in each room.',
-        'To see what spells you will have available when facing each enemy, look at the top of the screen. \n The portraits portray the enemies you will face and the icons below it indicate what spells you will have available then. \n This allows you to think ahead and plan the attack you wish to use against a given enemy. \n \n Or don\'t think ahead - I don\'t care.',
+        'A Slime blocks your way.',
+        'But before you choose what to attack with, take a look at your spells.',
+        'Notice Ice Spear is missing?. \n You don\'t always have the same attacks available in each room.',
+        'To see which attacks you will have available when facing each future enemy, look at the top of the screen. \n The portraits show the enemies you will face and the icons below indicate what spells you will have available. \n This allows you to think ahead and plan the attack you wish to use against a given enemy. \n \n Or don\'t - I don\'t care.',
         'Now I wish you good luck against the Slime! \n Because this is just the tutorial, whether you win or lose you will still move on to the next part after this Slime-battle. \n \n Have fun!',
     ],
     dk: [
-        'Velkommen til One-Shot Wizard! \n Mit navn er: Værner-Vejledningstekst og jeg vil lærer dig hvordan man spiller - klik venlist på pilen til højre for at fortsætte.',
-        'Det her er dig. Du er en Wizard (Altså en troldmand).',
-        'Det her er din modstander. Hvert rum i spillet har en.',
-        'For at besejre din modstander må du vælge et angreb fra de mulige. \n Nogle fjender er sårbare overfor bestemte angreb og det er op til dig at regne ud hvilket angreb der vil være effektivt mod din nuværende modstander \n \n Men vælg med omtanke! Fordi du får kun ET SKUD!',
-        'I øjeblikket er du mod en anden Wizard. \n Wizards er, som vi alle ved, meget svage og skrøbelige, så du skal ikke bekymre dig om hvilket angreb der er bedst - hvad som helst ville virke!',
-        'Vælg nu et af de tre magiske angreb.',
-        'Når du rammer en modstander vi de forsøge at modstå angrebet. \n Hvis den lilla indikator når toppen, vil din modstander overkomme din magi og angribe dig tilbage! \n \n Lad os se hvor godt den skrøbelige Wizard klarer sig...',
-        'Godt klaret! Du besejrede den gamle, svagelige troldmand. \n \n Meget imponerende... \n \n Når en modstander er besejret vil du automatisk bevæge dig til det næste rum - lad os tage derhen nu!',
+        'Velkommen til One-Hit Wizard! \n Mit navn er: Verner-Vejledningstekst og jeg vil lære dig hvordan man spiller - klik på pilen til højre for at fortsætte.',
+        'Det her er dig. Du er en troldmand. Troldmænd har enorme offensive egenskaber, men ingen defensive, hvilket du har sat dig for at ændre på. \n \n Du har hørt rygter om, at et magisk skjold ligger gemt i dette tempel. Et skjold der kan beskytte mod al skade - hvilket betyder det kunne stoppe en troldmand fra at dø af et enkelt slag, hvad de ellers er kendt for at gøre.',
+        'For at få fat i skjoldet må du besejre alle templets vogtere. Dette er en af dem. Hvert rum i spillet har én.',
+        'Det er op til dig at vælge det angreb ud af de mulige der kan besejre din nuværende modstander \n \n Men vælg med omtanke! For du får kun en chance...',
+        'I øjeblikket er du mod en ond troldmand. \n Grundet troldmænds manglende forsvar, afgøres dueller ofte af, hvem der angriber først. Heldigvis er det altid dig der starter i One-Hit Wizard.',
+        'Vælg nu et af de tre magiske angreb. De er alle velegnede til at ordne troldmænd.',
+        'Når du rammer en modstander vi de forsøge at modstå angrebet. \n Hvis den lilla indikator når toppen, vil din modstander overkomme din magi og angribe dig tilbage! \n \n Lad os se hvor godt den onde troldmand klarer sig...',
+        'Godt klaret! Du besejrede den onde troldmand! \n \n Når en modstander er besejret, vil du automatisk bevæge dig til det næste rum - lad os tage derhen nu!',
         'Velkommen til rum nummer to!',
-        'PAS PÅ!!! \n EN NY FJENDE NÆRMER SIG! \n \n I sandhed et afskyeligt væsen... Det må besejres!',
+        'En Slimklat blokerer vejen.',
         'Men før du beslutter dig for et angreb, så tag et kig på din magi.',
         'Bemærk at Issyl mangler. \n Du har ikke altid de samme angreb til rådighed i hvert rum.',
-        'Tjek toppen af skærmen for en oversigt over hvilke angreb du vil have til rådighed mod hver fremtidige modstander. \n Portrætterne viser modstanderen du vil møde og ikonerne nedenunder indikere hvilke angreb du vil have til rådighed mod netop dem. \n Dette giver du mulighed for at tænke forud og planlægge hvilket angreb du vil bruge mod dine fremtidige fjender. \n \n Eller du kan ignorere det - jeg er ligeglad.',
-        'Men nu vil jeg ønske dig farvel, samt held og lykke mod Slimklatten! \n Fordi dette bare er en introduktion betyder det ikke noget om du vinder eller taber. Du vil rykke videre til næste del uanset hvad. \n \n God fornøjelse!',
+        'Tjek toppen af skærmen for en oversigt over hvilke angreb du vil have til rådighed mod hver fremtidige modstander. \n Portrætterne viser modstanderen du vil møde og ikonerne nedenunder indikerer hvilke angreb du vil have til rådighed mod netop dem. \n Dette giver dig mulighed for at tænke forud, og planlægge hvilket angreb du vil bruge mod dine fremtidige fjender. \n \n Eller du kan ignorere det - jeg er ligeglad.',
+        'Men nu vil jeg ønske dig farvel, samt held og lykke mod Slimklatten! \n Da dette blot er en introduktion, betyder det ikke noget om du vinder eller taber. Du vil rykke videre til næste del uanset hvad. \n \n God fornøjelse!',
+    ]
+}
+
+enemyNames = {
+    en: {
+        wizard: "A Wizard",
+        skeleton: "A Skeleton",
+        mushroom: "A Fungrowth",
+        flyingEye: "An Eyesore",
+        goblin: "A Goblin",
+    },
+    dk: {
+        wizard: "En Troldmand",
+        skeleton: "Et Skelet",
+        mushroom: "En Champign-mand",
+        flyingEye: "Et Flagerøje",
+        goblin: "En Goblin",
+    }
+}
+
+approaches = {
+    en: [
+        " blocks your way.",
+        " approaches.",
+        " appears.",
+        " confronts you.",
+        " charges in to fight."
+    ],
+    dk: [
+        " blokere din vej.",
+        " gør klar til angreb.",
+        " nærmer sig",
+        " dukker op",
+        " konfrontere dig."
     ]
 }
